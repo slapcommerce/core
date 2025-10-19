98e450b3071ebbb2a350d22f4ca08bb7d4d5d073
Ryan Wible <ryanwiblecoding@gmail.com>
2025-10-19 10:58:53 -0600

    stable

--- src/integrationEvents/_base.ts
+++ src/integrationEvents/_base.ts
@@ -1,14 +1,10 @@
 export interface IntegrationEvent<
   Name extends string,
-  Payload extends Record<string, unknown>,
+  Payload extends Record<string, unknown>
 > {
   eventId: string;
   eventName: Name;
   occurredAt: Date;
   correlationId: string;
-  causationId: string;
-  aggregateId: string;
-  aggregateVersion: number;
-  routingKey: string;
   payload: Payload;
 }
